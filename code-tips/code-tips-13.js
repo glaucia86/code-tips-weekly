7e7a0bcff97028feba626350131cb1e08cc94ca5
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-19 13:33:38 -0300

    adicionando o Code Tips #13

---
+++ code-tips/code-tips-13.js
@@ -0,0 +1,22 @@
+/**
+ * Code Tips #13 - TypeScript/JavaScript
+ * (todas as quartas-feiras dicas de código!)
+ * Twitter: @glaucia_lemos86
+ * Lintree: linktree.ee/glaucia_lemos86
+ */
+
+// ==> push()
+
+/**
+ * Este método adiciona um ou mais elementos ao final de um determinado
+ * array e retorna o novo comprimento desse array
+ */
+
+// ==> Exemplo 01: uso simples do uso do método push() 
+
+let numeros = ['1️⃣', '2️⃣', '3️⃣', '4️⃣'];
+numeros.push('5️⃣');
+
+console.log(numeros);
+// ==> Resultado:
+// [ '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣' ]
